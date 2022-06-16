0ad944676be49f15ca0c1185e67980ba6438b475
Adebisi Adebayo Paul <ollypapi1@gmail.com>
2022-06-16 16:21:11 +0100

    Workservice adjustment

---
+++ WorkPlanService/Models/Worker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkPlanService.Models
+{
+    public class Worker
+    {
+    }
+}
---
+++ WorkPlanService/Services/WorkService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkPlanService.Services
+{
+    public class WorkService:IWorkService
+    {
+    }
+}
